75a369197026fcd2f5bbd27a5c37f6a150b7982d
w1skasss <144696777+w1skasss@users.noreply.github.com>
2023-10-30 17:17:52 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,149 +1,148 @@
 // лепилова шленова 821
 
-// № 1
-// let elem = document.querySelector('#input');
-// let input = document.querySelector('#elem');
-// elem.addEventListener('blur', function() { 
-//   let elem1 = elem.value;
-//   input.textContent += ' ' + elem1;
-//   console.log(input.textContent);
+// 1
+// let input = document.getElementById("input");
+// let elem = document.getElementById("elem");
+// input.addEventListener("blur", function() {
+//     elem.textContent += input.value;
 // });
 
-// № 2
-// document.querySelector('#buttonSum').addEventListener('click', function() {
-//     let input1 = parseInt(document.querySelector('#input1').value);
-//     let input2 = parseInt(document.querySelector('#input2').value);
-//     let input3 = parseInt(document.querySelector('#input3').value);
-//     let sum = (isNaN(input1) ? 0 : input1) + (isNaN(input2) ? 0 : input2) + (isNaN(input3) ? 0 : input3);
-//     let result = document.querySelector('#result');
-//     result.textContent = `${sum}`;
-//     console.log(`${sum}`);
+// 2
+// let input1 = document.getElementById("input1");
+// let input2 = document.getElementById("input2");
+// let input3 = document.getElementById("input3");
+// let elem = document.getElementById("elem");
+// let button = document.getElementById("button");
+// button.addEventListener("click", function() {
+//     let sum = Number(input1.value) + Number(input2.value) + Number(input3.value);
+//     elem.textContent = "Сумма: " + sum;
 // });
 
-// № 3
-// let input = document.getElementById('input');
-// let result = document.getElementById('result');
-// input.addEventListener('blur', function() {
+// 3
+// let input = document.getElementById("input");
+// let sumElem = document.getElementById("sumElem");
+// input.addEventListener("blur", function() {
 //     let number = input.value;
 //     let sum = 0;
 //     for (let i = 0; i < number.length; i++) {
 //         sum += parseInt(number.charAt(i));
 //     }
-//     result.textContent = 'Сумма цифр числа: ' + sum;
+//     sumElem.textContent = "Сумма: " + sum;
 // });
 
-// № 4
-// document.getElementById('input').addEventListener('blur', function() {
-//     let input = this.value;
-//     let number = input.split(',');
+// 4
+// let input = document.getElementById("input");
+// let averageElem = document.getElementById("averageElem");
+// input.addEventListener("blur", function() {
+//     let numbers = input.value.split(",");
 //     let sum = 0;
-//     for (let i = 0; i < number.length; i++) {
-//         sum += parseInt(number[i]);
+//     for (let i = 0; i < numbers.length; i++) {
+//         sum += parseInt(numbers[i]);
 //     }
-//     let average = sum / number.length;
-//     result.textContent = 'Среднее арифметическое: ' + average;
+//     let average = sum / numbers.length;
+//     averageElem.textContent = "Среднее арифметическое: " + average;
 // });
 
-// № 5
-// document.getElementById('fioInput').addEventListener('blur', function() {
-//     let fioInput = this.value.split(' ');
-//     if (fioInput.length >= 3) {
-//         document.getElementById('surnameInput').value = fioInput[0];
-//         document.getElementById('nameInput').value = fioInput[1];
-//         document.getElementById('patronymicInput').value = fioInput[2];
-//     } else {
-//         alert('Введите ваше ФИО полностью');
-//     }
+// 5
+// let fioInput = document.getElementById("fioInput");
+// let surnameInput = document.getElementById("surnameInput");
+// let nameInput = document.getElementById("nameInput");
+// let patronymicInput = document.getElementById("patronymicInput");
+// fioInput.addEventListener("blur", function() {
+//     let fullName = fioInput.value.split(" ");
+//     surnameInput.value = fullName[0];
+//     nameInput.value = fullName[1];
+//     patronymicInput.value = fullName[2];
 // });
 
-// № 6
-// document.querySelector('#fioInput').addEventListener('blur', function() {
-//     let fioInput = this.value.split(' ');
-//     if (fioInput.length >= 3) {
-//         let input1 = capitalizeFirstLetter(fioInput[0]);
-//         let input2 = capitalizeFirstLetter(fioInput[1]);
-//         let input3 = capitalizeFirstLetter(fioInput[2]);
-//         this.value = `${input1} ${input2} ${input3}`;
-//     } else {
-//         alert('Введите ФИО полностью');
-//     }
+// 6
+// let input = document.getElementById("input");
+// input.addEventListener("blur", function() {
+//     let text = input.value;
+//     let words = text.split(" ");
+//     let formattedText = words.map(function(word) {
+//         return word.charAt(0).toUpperCase() + word.slice(1);
+//     }).join(" ");
+//     input.value = formattedText;
 // });
 
-// function capitalizeFirstLetter(string) {
-//   return string.charAt(0).toUpperCase() + string.slice(1);
-// }
-
-// № 7
-// document.querySelector('#textInput').addEventListener('blur', function() {
-//     let text = textInput.value;
-//     let words = text.split(' ');
-//     let count = words.filter(word => word !== '').length;
-//     let wordCount = document.querySelector('#wordCount');
+// 7
+// let input = document.getElementById("input");
+// let wordCount = document.getElementById("wordCount");
+// input.addEventListener("blur", function() {
+//     let text = input.value;
+//     let words = text.split(" ");
+//     words = words.filter(function(word) {
+//         return word !== "";
+//     });
+//     let count = words.length;
 //     wordCount.textContent = "Количество слов: " + count;
 // });
 
-// № 8
-// document.querySelector('#dateInput').addEventListener('blur', function() {
-//     let dateInput = this.value;
-//     let part = dateInput.split('.');
-//     if (part.length === 3) {
-//         let elem = `${part[2]}-${part[1]}-${part[0]}`;
-//         this.value = elem;
+// 8
+// let input = document.getElementById("input");
+// input.addEventListener("blur", function() {
+//     let input = input.value;
+//     let parts = input.split(".");
+//     let formattedDate = parts[2] + "-" + parts[1] + "-" + parts[0];
+//     input.value = formattedDate;
+// });
+
+// 9
+// let input = document.getElementById("input");
+// let checkButton = document.getElementById("checkButton");
+// let result = document.getElementById("result");
+// checkButton.addEventListener("click", function() {
+//     let word = input.value.toLowerCase();
+//     if (word === word.split("").reverse().join("")) {
+//         result.textContent = "палиндром";
 //     } else {
-//         alert('Введите дату в формате 31.12.2016');
+//         result.textContent = "не палиндром";
 //     }
 // });
 
-// № 9
-// document.querySelector('#wordInput').addEventListener('blur', function() {
-//     let word = this.value.toLowerCase();
-//     let wordInput = word.split('').reverse().join('');
-//     if (word === wordInput) {
-//         result.textContent = "слово читается с начала и с конца одинаково";
-//     } else {
-//         result.textContent = "слово читается с начала и с конца по-разному";
-//     }
-// });
-
-// № 10
-// document.querySelector('#numberInput').addEventListener('blur', function() {
-//     let number = this.value;
-//     if (number.includes('3')) {
+// 10
+// let input = document.getElementById("input");
+// let result = document.getElementById("result");
+// input.addEventListener("blur", function() {
+//     let number = input.value;
+//     if (number.includes("3")) {
 //         result.textContent = "Число содержит внутри себя цифру 3";
 //     } else {
 //         result.textContent = "Число не содержит внутри себя цифру 3";
 //     }
 // });
 
-// № 11
-// document.querySelector('#elem').addEventListener('click', function() {
-//     let elem = document.querySelectorAll('p');
-//     for (let i = 0; i < elem.length; i++) {
-//         elem[i].textContent += " " + (i + 1);
+// 11
+// let paragraphs = document.getElementsByTagName("p");
+// let button = document.getElementById("button");
+// button.addEventListener("click", function() {
+//     for (let i = 0; i < paragraphs.length; i++) {
+//         paragraphs[i].textContent += " " + (i + 1);
 //     }
 // });
 
-// № 12
-// document.getElementById('elem').addEventListener('click', function() {
-//     let links = document.querySelectorAll('a');
-//     links.forEach(function(link) {
-//         let href = link.getAttribute('href');
-//         link.textContent += ` (${href})`;
-//     });
+// 12
+// let links = document.getElementsByTagName("a");
+// let button = document.getElementById("button");
+// button.addEventListener("click", function() {
+//     for (let i = 0; i < links.length; i++) {
+//         links[i].textContent += " (" + links[i].href + ")";
+//     }
 // });
 
-// № 13
-// document.getElementById('elem').addEventListener('click', function() {
-//    let links = document.querySelectorAll('a');
-//     links.forEach(function(link) {
-//         let href = link.getAttribute('href');
-//         if (href.startsWith('http://')) {
-//             link.textContent += '→';
+// 13
+// let links = document.getElementsByTagName("a");
+// let button = document.getElementById("button");
+// button.addEventListener("click", function() {
+//     for (let i = 0; i < links.length; i++) {
+//         if (links[i].href.startsWith("http://")) {
+//             links[i].textContent += " →";
 //         }
-//     });
+//     }
 // });
 
-// № 14
+// 14
 // let number1 = document.getElementById("number1");
 // let number2 = document.getElementById("number2");
 // let number3 = document.getElementById("number3");
@@ -161,11 +160,11 @@
 //     number3.textContent = square;
 // });
 
-// № 15
-// let dateInput = document.getElementById("dateInput");
+// 15
+// let input = document.getElementById("input");
 // let result = document.getElementById("result");
-// dateInput.addEventListener("blur", function() {
-//     let date = dateInput.value;
+// input.addEventListener("blur", function() {
+//     let date = input.value;
 //     let dayOfWeek = getDayOfWeek(date);
 //     result.textContent = dayOfWeek;
 // });
@@ -179,32 +178,32 @@
 //     return daysOfWeek[date.getDay()];
 // }
 
-// № 16
+// 16
 // let input = document.getElementById("input");
 // let button1 = document.getElementById("button1");
 // let button2 = document.getElementById("button2");
-// button1.addEventListener('click', function() {
-//     let elem = parseInt(input.value);
-//     input.value = elem + 1;
+// button1.addEventListener("click", function() {
+//     let currentValue = parseInt(input.value);
+//     input.value = currentValue + 1;
 // });
-// button2.addEventListener('click', function() {
-//     let elem = parseInt(input.value);
-//     if (elem > 0) {
-//         input.value = elem - 1;
+// button2.addEventListener("click", function() {
+//     let currentValue = parseInt(input.value);
+//     if (currentValue > 0) {
+//         input.value = currentValue - 1;
 //     }
 // });
 
-// № 17
-// let numberInput = document.getElementById('numberInput');
-// let elem = document.getElementsByTagName('p');
-// for (let i = 0; i < elem.length; i++) {
-//     elem[i].addEventListener("click", function() {
-//         let numberr = parseInt(numberInput.value);
-//         numberInput.value = numberr + 1;
+// 17
+// let input = document.getElementById("input");
+// let paragraphs = document.getElementsByTagName("p");
+// for (let i = 0; i < paragraphs.length; i++) {
+//     paragraphs[i].addEventListener("click", function() {
+//         let currentValue = parseInt(input.value);
+//         input.value = currentValue + 1;
 //     });
 // }
 
-// № 18
+// 18
 // let divs = document.getElementsByTagName("div");
 // for (let i = 0; i < divs.length; i++) {
 //     let div = divs[i];
@@ -215,7 +214,7 @@
 //     div.innerHTML = text;
 // }
 
-// № 19
+// 19
 // function generateRandomString() {
 //     let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 //     let randomString = "";
@@ -223,12 +222,12 @@
 //         let randomIndex = Math.floor(Math.random() * characters.length);
 //         randomString += characters.charAt(randomIndex);
 //     }
-//     document.getElementById("randomStringInput").value = randomString;
+//     document.getElementById("randomInput").value = randomString;
 // }
 
-// № 20
+// 20
 // function generateRandomString() {
-//     let input = document.getElementById("randomStringInput");
+//     let input = document.getElementById("randomInput");
 //     let string = input.value;
 //     let shuffledString = shuffleString(string);
 // input.value = shuffledString;
@@ -244,20 +243,20 @@
 //     return characters.join("");
 // }
 
-// № 21
+// 21
 // function convertToFahrenheit() {
-//     let fahrenheitInput = document.getElementById("fahrenheitInput");
+//     let inputF = document.getElementById("inputF");
 //     let celsiusResult = document.getElementById("result");
-//     let fahrenheit = parseFloat(fahrenheitInput.value);
+//     let fahrenheit = parseFloat(inputF.value);
 //     let celsius = (fahrenheit - 32) * 5/9;
-//     celsiusResult.textContent = "Temperature in Celsius: " + celsius.toFixed(2);
+//     celsiusResult.textContent = "Температура цельсия: " + celsius.toFixed(2);
 // }
 
-// № 22
+// 22
 // function calculateFactorial() {
-//     let numberInput = document.getElementById("numberInput");
+//     let input = document.getElementById("input");
 //     let result = document.getElementById("result");
-//     let number = parseInt(numberInput.value);
+//     let number = parseInt(input.value);
 //     let factorial = 1;
 //     for (let i = 1; i <= number; i++) {
 //         factorial *= i;
@@ -265,15 +264,15 @@
 //     result.textContent = "Factorial: " + factorial;
 // }
 
-// № 23
+// 23
 // function calculateRoots() {
-//     let inputA = document.getElementById("inputA");
-//     let inputB = document.getElementById("inputB");
-//     let inputC = document.getElementById("inputC");
+//     let input1 = document.getElementById("input1");
+//     let input2 = document.getElementById("input2");
+//     let input3 = document.getElementById("input3");
 //     let result = document.getElementById("result");
-//     let a = parseInt(inputA.value);
-//     let b = parseInt(inputB.value);
-//     let c = parseInt(inputC.value);
+//     let a = parseInt(input1.value);
+//     let b = parseInt(input2.value);
+//     let c = parseInt(input3.value);
 //     let discriminant = Math.pow(b, 2) - (4 * a * c);
 //     if (discriminant > 0) {
 //         let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
@@ -281,8 +280,8 @@
 //         result.textContent = "Roots: " + root1 + ", " + root2;
 //     } else if (discriminant === 0) {
 //         let root = -b / (2 * a);
-//         result.textContent = "Root: " + root;
+//         result.textContent = "корень: " + root;
 //     } else {
-//         result.textContent = "No real roots";
+//         result.textContent = "нет корней";
 //     }
 // }
